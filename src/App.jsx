7bfac3387f0768504d2c85f83a2799d3ffdf0451
edy1013 <32241060+edy1013@users.noreply.github.com>
2021-10-20 21:59:17 +0200

    About section start

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import About from "./components/intro/about/About.jsx";
 import Intro from "./components/intro/Intro.jsx";
 
 const App = () => {
-  return <div><Intro /></div>;
+  return (
+    <div>
+      <Intro />
+      <About />
+  </div>
+  );
 };
 
 export default App;
---
+++ src/components/intro/about/About.jsx
@@ -0,0 +1,19 @@
+import "./about.css"
+
+const about = () => {
+    return (
+        <div className="a">
+            <div className="a-left">
+                <div className="a-card bg">
+                    
+                </div>
+                <div className="a-card">
+                <img src="https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="a-img"/>
+                </div>
+            </div>
+            <div className="a-right"></div>
+        </div>
+    )
+}
+
+export default about
