b496b4d6f5ac40765080dd1e6d8a9c0f4f9f58a9
edy1013 <32241060+edy1013@users.noreply.github.com>
2021-11-29 21:47:29 +0100

    Update about me

--- src/components/intro/about/About.jsx
+++ src/components/intro/about/About.jsx
@@ -14,7 +14,7 @@ const about = () => {
             <div className="a-right">
                  <h1 className="a-title">About Me</h1>
                      <p className="a-sub">
-                        Ada is my Love.
+                        
                      </p>
                     <p className="a-desc">
                     I’m a self thoutgh FrontEnd web developer. I specialize in front end development, user experience, and creating identities and branding. Looking to work and learn , so contact me if you’d like to work together on your next project.
